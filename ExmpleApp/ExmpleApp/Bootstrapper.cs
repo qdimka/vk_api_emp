c3ebe5b6a45e6186adb9bd46840e35a9b044967a
qdimka <qdimka1991@mail.ru>
2016-03-05 21:03:28 +0300

    Заготовка

---
+++ ExmpleApp/ExmpleApp/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using ExmpleApp.Views;
+using Prism.Mef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace ExmpleApp
+{
+    public class Bootstrapper : MefBootstrapper
+    {
+        //Создание загрузчика для приложения
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.GetExportedValue<MainWindow>();
+        }
+
+        //Инициализация
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (MainWindow)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        //Обязательно переопределить для использования Mef
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Bootstrapper).Assembly));
+        }
+    }
+}
