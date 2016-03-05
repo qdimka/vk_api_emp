a44adc60a8c16e5f9626e7c71e1df9bebbf938e6
qdimka <qdimka1991@mail.ru>
2016-03-05 22:33:18 +0300

    Добавил вьюшку

---
+++ ExmpleApp/ExmpleApp.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExmpleApp.Infrastructure
+{
+    public static class RegionNames
+    {
+        public const String MainRegion = "MainRegion";
+        public const String PlayerRegion = "PlayerRegion";
+    }
+}
