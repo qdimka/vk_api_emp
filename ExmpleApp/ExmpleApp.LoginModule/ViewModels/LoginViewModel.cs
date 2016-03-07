1356ac75f07e1196165f82739152e0ee2f733299
qdimka <qdimka1991@mail.ru>
2016-03-07 18:05:06 +0300

    Добавил команду

--- ExmpleApp/ExmpleApp.LoginModule/ViewModels/LoginViewModel.cs
+++ ExmpleApp/ExmpleApp.LoginModule/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ExmpleApp.LoginModule.Models;
 using ExmpleApp.LoginModule.Services;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
@@ -8,6 +9,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ExmpleApp.LoginModule.ViewModels
 {
@@ -15,13 +17,16 @@ namespace ExmpleApp.LoginModule.ViewModels
     [PartCreationPolicy(CreationPolicy.NonShared)]
     public class LoginViewModel : BindableBase
     {
+        private readonly DelegateCommand loginCommand;
         private readonly IloginService loginService;
         private LoginModel login;
 
         [ImportingConstructor]
         public LoginViewModel(IloginService LoginService)
         {
+            this.login = new LoginModel();
             this.loginService = LoginService;
+            this.loginCommand = new DelegateCommand(LogIn);
         }
 
         public LoginModel Login
@@ -30,5 +35,14 @@ namespace ExmpleApp.LoginModule.ViewModels
             set { this.SetProperty(ref this.login, value); }
         }
 
+        public ICommand LoginCommand
+        {
+            get { return loginCommand; }
+        }
+
+        private void LogIn()
+        {
+            loginService.Login(Login);
+        }
     }
 }
