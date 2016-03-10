d130ad53c3d9564ef1b2b0d6c921f9702f23fd53
qdimka <qdimka1991@mail.ru>
2016-03-10 13:41:41 +0300

    Добавил вьюшку

--- ExmpleApp/ExmpleApp.PlayerModule/ViewModels/PlayListViewModel.cs
+++ ExmpleApp/ExmpleApp.PlayerModule/ViewModels/PlayListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using ExmpleApp.Infrastructure.SharedServices;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,28 +8,75 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using VkNet.Model.Attachments;
 
 namespace ExmpleApp.PlayerModule.ViewModels
 {
     [Export]
     [PartCreationPolicy(CreationPolicy.NonShared)]
-    public class PlayListViewModel
+    public class PlayListViewModel:BindableBase
     {
         IVkAudioService _audioService;
-        ReadOnlyCollection<Audio> _music;
+        ReadOnlyCollection<Audio> music;
+        private string query;
+
+        private readonly DelegateCommand getPopular;
+        private readonly DelegateCommand getRecommend;
+        private readonly DelegateCommand getSearch;
+        private readonly DelegateCommand getUserMusic;
 
         [ImportingConstructor]
         public PlayListViewModel(IVkAudioService audioService, IVkApi api)
         {
             this._audioService = audioService;
-            _music = this._audioService.GetMusicByUserId(api.Instance.UserId);
+            music = this._audioService.GetMusicByUserId(api.Instance.UserId);
+
+            this.getPopular = new DelegateCommand(GetPopular);
+            this.getRecommend = new DelegateCommand(GetRecommend);
+            this.getSearch = new DelegateCommand(GetSearch);
+            this.getUserMusic = new DelegateCommand(GetUserMusic);
         }
 
-        //ObservableCollection<Audio> Music
-        //{
-        //    get { return }
-        //    set { this.SetProperty(ref this.login, value); }
-        //}
+
+        ReadOnlyCollection<Audio> Music
+        {
+            get { return this.music; }
+            set { this.SetProperty(ref this.music, value); }
+        }
+
+        public String SearchQuery
+        {
+            get { return this.query; }
+            set { this.SetProperty(ref this.query, value); }
+        }
+
+        public ICommand GetPopularMusic => getPopular;
+
+        public ICommand GetRecommendMusic => getRecommend;
+
+        public ICommand GetSearchMusic => getSearch;
+
+        public ICommand GetMusicByUser => getUserMusic;
+
+        private void GetPopular()
+        {
+            Music = this._audioService.GetPopularMusic();
+        }
+
+        private void GetRecommend()
+        {
+            Music = this._audioService.GetPopularMusic();
+        }
+
+        private void GetSearch()
+        {
+            Music = this._audioService.GetPopularMusic();
+        }
+
+        private void GetUserMusic()
+        {
+            Music = this._audioService.GetPopularMusic();
+        }
     }
 }
---
+++ ExmpleApp/ExmpleApp.PlayerModule/Views/PlayListView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ExmpleApp.PlayerModule.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExmpleApp.PlayerModule.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для PlayListView.xaml
+    /// </summary>
+    [Export(typeof(PlayListView))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class PlayListView : UserControl
+    {
+        public PlayListView()
+        {
+            InitializeComponent();
+        }
+
+        [Import]
+        public PlayListViewModel ViewModel
+        {
+            get { return this.DataContext as PlayListViewModel; }
+            set { this.DataContext = value; }
+        }
+    }
+}
