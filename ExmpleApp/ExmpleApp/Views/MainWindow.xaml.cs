8991182db11918857384694abb93a4e6527e8bdd
qdimka <qdimka1991@mail.ru>
2016-03-14 23:01:33 +0300

    Подцепил MahApps

--- ExmpleApp/ExmpleApp/Views/MainWindow.xaml.cs
+++ ExmpleApp/ExmpleApp/Views/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace ExmpleApp.Views
 {
@@ -23,7 +24,7 @@ namespace ExmpleApp.Views
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     [Export]
-    public partial class MainWindow : Window, IPartImportsSatisfiedNotification
+    public partial class MainWindow : MetroWindow, IPartImportsSatisfiedNotification
     {
         private const string LoginModuleName = "LoginModule";
         private static Uri LoginViewUri = new Uri(@"/LoginView", UriKind.Relative);
